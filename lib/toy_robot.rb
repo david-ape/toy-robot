ee68c52d34b2f82b095932203d2f79704d88dbc2
david-ape <david@planethayward.com>
2022-07-30 18:57:08 +0800

    Refactor cli handling
    Make code easier to follow (and more OOish and SOLIDish) by refactoring to use Cli and Cli:Command classes.

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -2,7 +2,15 @@ require "toy_robot/version"
 require "toy_robot/robot"
 
 module ToyRobot
-  class Error < StandardError; end
+  # This is the main entry point
+  #
+  # The io argument is a hack for testing. I'm
+  # specifying a StringIO in my rspec tests
+  # until I can figure out how to test
+  # with STDIN
+  def self.run(argv=[], io=STDIN)
+    Cli.new(argv).run(io)
+  end
 
   HELP_MSG = <<~EOS
     This app lets you control a toy robot on a table top. The table top is broken
@@ -30,79 +38,110 @@ module ToyRobot
     Enjoy :-)
   EOS
 
-  # This is the main entry point
-  #
-  # The io argument is a hack for testing. I'm
-  # specifying a StringIO in my rspec tests
-  # until I can figure out how to test
-  # with STDIN
-  def self.run(argv=[], io=STDIN)
-    # Clear instance vars in case we are running multiple times
-    @robot = nil
-    @name = nil
-    @verbose = nil
+  class Error < StandardError; end
 
-    process_args(argv)
+  class Cli
+    def initialize(argv)
+      process_args(argv)
+    end
 
-    io.each do |command|
-      # Add some quit commands (^D will also work)
-      # If we wanted it tighter, then we'd just have one quit command
-      if ['quit','exit','stop','bye'].include? command.strip.downcase
-        exit 0
+    def run(io)
+      io.each do |command|
+        execute_command(command)
       end
-      # And give the user a way to get help
-      # Again if we wanted it tighter, then we'd lose the wtfs and just have help
-      if ['help','wtf','wtf?'].include? command.strip.downcase
-        puts HELP_MSG
-      else
-        # Otherwise we start commanding the robot to do stuff
-        # if their is a non-empty command.
-        unless command.strip.empty?
-          success, explanation = robot.obey(command)
-          puts explanation if verbose || !success
+    end
+
+    private
+
+    def process_args(argv)
+      argv.each_with_index do |option, i|
+        case option.downcase
+        when "-h", "--help"
+          puts
+          puts "toy_robot #{ToyRobot::VERSION}"
+          puts "Usage: toy_robot [options] [<file]"
+          puts "  -h, --help             Show usage"
+          puts "  -n, --name YOUR NAME   How the robot should address you"
+          puts "  -v, --verbose          Enable verbose mode"
+          puts
+          puts HELP_MSG
+          exit
+        when "-v", "--verbose"
+          @verbose = true
+        when '-n', '--name'
+          @name = argv[i+1]
+        else
+          # TODO: Raise or report an unrecognised argument error.
+          #       (Would need to make the arg processing a little
+          #       more sophisticated - maybe use an arg processing
+          #       gem.)
         end
       end
     end
-  end
 
-  def self.process_args(argv)
-    argv.each_with_index do |option, i|
-      case option.downcase
-      when "-h", "--help"
-        puts
-        puts "toy_robot #{ToyRobot::VERSION}"
-        puts "Usage: toy_robot [options] [<file]"
-        puts "  -h, --help             Show usage"
-        puts "  -n, --name YOUR NAME   How the robot should address you"
-        puts "  -v, --verbose          Enable verbose mode"
-        puts
-        puts HELP_MSG
-        exit
-      when "-v", "--verbose"
-        @verbose = true
-      when '-n', '--name'
-        @name = argv[i+1]
-      else
-        # TODO: Raise or report an unrecognised argument error.
-        #       (Would need to make the arg processing a little
-        #       sophisticated.)
+    def execute_command(command_string)
+      # Attempt to execute as a CLI command, otherwise
+      # send the command to the robot
+      unless CliCommand.new(command_string).execute
+        success, explanation = robot.obey(command_string)
+        puts explanation if verbose || !success
+      end
+    end
+
+    # Create the robot if needed, otherwise just return it (i.e. create lazily)
+    def robot
+      @robot ||= Robot.new(name)
+    end
+
+    def name
+      # If you want to risk being politically incorrect you could make the default
+      # Master or Mistress (but one day the robots will rise up :-)
+      @name ||= 'Commander'
+    end
+
+    def verbose
+      @verbose
+    end
+
+    # A class for commands handled by the CLI (rather than the robot)
+    class CliCommand
+      def initialize(raw_command)
+        @command = (raw_command || '').strip
+      end
+
+      def execute
+        if valid?
+          if quit?
+            exit 0
+          elsif help?
+            puts HELP_MSG
+          else
+            # ignore empty commands
+          end
+          true
+        else
+          false
+        end
+      end
+
+      private
+
+      def valid?
+        @valid ||= (empty? || quit? || help?)
+      end
+
+      def empty?
+        @empty ||= @command.empty?
+      end
+
+      def quit?
+        @quit ||= @command.downcase.match?(/^(quit|exit|stop|bye)$/)
+      end
+
+      def help?
+        # help, wtf, or wtf?
+        @help ||= @command.downcase.match?(/^(help|wtf\??)$/)
       end
     end
   end
-
-  # Create the robot if needed, otherwise just return it (i.e. create lazily)
-  def self.robot
-    @robot ||= Robot.new(name)
-  end
-
-  def self.name
-    # If you want to risk being politically incorrect you could make the default
-    # Master or Mistress (but one day the robots will rise up :-)
-    @name ||= 'Commander'
-  end
-
-  def self.verbose
-    @verbose
-  end
-
 end
